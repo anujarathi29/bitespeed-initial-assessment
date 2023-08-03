9933321a3419194ea31f1b98b2151a075b3cd1fa
anujarathi29 <anujarathi2@gmail.com>
2023-08-03 17:12:37 +0530

    Initial solution for Bitespeed assessment TBD: Dockerization and README

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import Identify from  "./components/Identify"
+import{useState} from "react"
+
+function App() {
+  
+  return (
+    <div>
+      <Identify />
+   </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/components/Identify.js
@@ -0,0 +1,48 @@
+import "./style.css"
+import { useEffect, useState } from "react"
+function Identify(props){
+
+    const [contact, setContact] = useState();
+
+    const handleSubmit = function (event) {
+        event.preventDefault()
+        var data = {
+            email: event.target.email.value,
+            phoneNumber: event.target.phoneNumber.value,
+        }
+        // console.log(data)
+		fetch("/identify", {
+			method :"POST",
+			body : JSON.stringify(data),
+			headers : {
+				"Content-type": "application/json"
+			}
+		}).then(res => res.json())
+		.then(data =>{
+            setContact(data);
+		})
+		.catch(err => console.log(err))
+    }
+
+return (
+    <div className="container-fluid m-5 p-5 d-flex align-items-center justify-content-center bg-light" style={{minHeight:"100vh"}}>
+
+        <form onSubmit={handleSubmit}>
+            <div className="input-group mb-5">
+            <input type="text" className="form-control border border-1 border-light shadow" placeholder="email" id="email"/>
+            <input type="text" className="form-control border border-1 border-light shadow" placeholder="phoneNumber" id="phoneNumber"/>
+            <button className="btn btn-outline-danger rounded-2 border border-0 shadow">Identify</button>
+            </div>
+            <span className="fw-light">
+                <pre>
+                    {JSON.stringify(contact, null, "\t")}
+                </pre>
+            </span>
+        </form>
+        
+    </div>
+   
+);
+}
+
+export default Identify;
