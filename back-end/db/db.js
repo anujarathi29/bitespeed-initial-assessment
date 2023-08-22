0d6cc28f36dda8c3be1108af99551f1c2ea9ffdc
anujarathi29 <anujarathi2@gmail.com>
2023-08-22 20:10:17 +0530

    use corect ports

--- back-end/db/db.js
+++ back-end/db/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://database:27018/database")
+mongoose.connect("mongodb://database:27017/database")
     .then(() => { console.log("connected to the database"); })
     .catch((err) => console.log("Failed to connect to the database", err))
